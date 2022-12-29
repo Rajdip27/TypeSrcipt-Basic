f6fab183bb83a5ea477fd3ffc969410e0def79e3
Raj <srajdip920@gmail.com>
2022-12-30 01:25:19 +0600

    2nd time paratic typescript

---
+++ New folder/MyTypescript/output/main.js
@@ -0,0 +1,15 @@
+"use strict";
+// // const Conutry = " I Love  Bangladesh";
+// // console.log(Conutry);
+// // let playerName;
+// // playerName="Sakib";
+// // playerName=35;
+// // console.log(playerName);
+// // function add(a:number,b:number){
+// //     return a+b;
+// // }
+// // console.log(add(25,45));
+// let Fruits=['apple','Mango','banana','oranage'];
+// // Fruits.push(45);
+// Fruits.push("Mango2");
+// console.log(Fruits )
--- output/main.js
+++ output/main.js
@@ -1,162 +1,190 @@
-const cuntory = "I love Bangladesh";
-console.log(cuntory);
-let playerName = "Sakib";
-let age = 35;
-console.log(playerName);
-age = 25;
-console.log(playerName);
-let playerName;
-playerName = "Sakib";
-playerName = 34;
-console.log(playerName);
-function mul(a, b) {
-    return a * b;
-}
-(array !== null && array !== void 0 ? array : ) ? ////
-    console.log(mul(12, 4))
-    :
-;
-let fruits = ['apple', 'banana', 'orange'];
-// fruits.push('mango');
-fruits.push(455);
-console.log(fruits);
-let mixed = ['apple', 3, true];
-mixed.push(45);
-mixed.push('Rajdip');
-mixed.push(false);
-console.log(mixed);
-let mixed = ['apple', 'Mango', true, {}, 4666];
-mixed.push({
-    name: "Rajdip"
-});
-console.log(mixed);
-//object//____
-let person = {
-    name: "Sakib",
-    age: 62,
-    IsCaptain: true,
-};
-person.name = "Mashrafi";
-console.log(person);
-let a;
-let b;
-a = "kdfjdf";
-b = "kjfdsjdf";
-let a = [];
-a.push(332);
-a.push("hjadfjdfa");
-console.log(a);
-let a;
-//a="kjdfhhfdj";
-a = 44564;
-object;
-let c;
-c = {
-    name: "fdshdsgf",
-    age: 236,
-    male: true,
-};
-console.log(c);
-let c;
-c = {
-    name: "Raj",
-    age: 25,
-    adult: true,
-};
-console.log(c);
-let c;
-c = [1, 2, 3];
-let a;
-a = 5;
-a = "hfds";
-let a = [];
-a.push(6544);
-a.push("kjsdkfjkfdjs");
-console.log(a);
-let b;
-b = {
-    name: 656545,
-    age: 4545,
-};
-console.log(b);
-const myfun = () => {
-    console.log("hello");
-};
-console.log(myfun());
-let myFun;
-myFun = () => {
-    console.log("Hello");
-};
-myFun = 5;
-const MyFunc = (a, b, c = "ture") => {
-    return (`hello ${a} ${b}`);
-};
-console.log(MyFunc("Rajdip", "Raj"));
-let UserDetails = (id, user) => {
-    console.log(`User Id is${id},name is ${user.name} and Age ${user.age}`);
-};
-let sayHello = (user) => {
-    console.log(`hello${user.age > 50 ? "sir" : "Mr."}${user.name}`);
-};
-let Add;
-Add = (m, n) => {
-    return m + n;
-};
-console.log(Add(45, 20));
-let Calculation;
-Calculation = (a, b, c) => {
-    if (c == 'add') {
-        return a + b;
-    }
-    else {
-        return a - b;
-    }
-};
-console.log(Calculation(45, 20, 'add'));
-let UserDetails;
-UserDetails = (id, userinfo) => {
-    return `User Id :${id} UserName${userinfo.name},User Age :${userinfo.age}`;
-};
-console.log(UserDetails(1, "RAj", 45));
-class player {
-    constructor(n, a, c) {
-        this.name = n,
-            this.age = a,
-            this.Country = c;
-    }
-    play() {
-        console.log(`${this.name}from ${this.Country} age ${this.age}`);
-    }
-}
-const Mashrafi = new player("Sakib", 40, "Bangladesh");
-const Sakib = new player("Mashrafi", 35, "Bangladesh");
-const players = [];
-players.push(Sakib);
-players.push(Mashrafi);
-console.log(players);
-class player {
-    constructor(name, age, Country) {
-        this.name = name;
-        this.age = age;
-        this.Country = Country;
-    }
-    play() {
-        console.log(`${this.name}from ${this.Country} age ${this.age}`);
-    }
-}
-const Mashrafi = new player("Sakib", 40, "Bangladesh");
-let Sakib;
-Sakib = new player("abc", 24, "Bangladesh");
-const players = [];
-players.push(Sakib);
-players.push(Mashrafi);
-console.log(players);
-function drawRectangle(options) {
-    let width = options.width;
-    let length = options.length;
-}
-drawRectangle({
-    width: 30,
-    length: 20,
-});
-export {};
+"use strict";
+// const cuntory="I love Bangladesh";
+// console.log(cuntory);
+// let playerName="Sakib";
+// let age=35
+// console.log(playerName);
+// age=25;
+// console.log(playerName);
+// let playerName;
+// playerName="Sakib";
+// playerName=34;
+// console.log(playerName);
+// function mul(a:number,b:number){
+//     return a*b;
+// }
+// array???////
+// console.log(mul(12,4))
+// let fruits=['apple','banana','orange'];
+// // fruits.push('mango');
+// fruits.push(455);
+// console.log(fruits);
+// let mixed=['apple',3,true]
+// mixed.push(45);
+// mixed.push('Rajdip');
+// mixed.push(false);
+// console.log(mixed)
+// let mixed=['apple','Mango',true,{},4666]
+// mixed.push({
+//     name:"Rajdip"
+// })
+// console.log(mixed);
+// //object//____
+// let person={
+//     name:"Sakib",
+//     age:62,
+//     IsCaptain:true,
+// }
+// person.name="Mashrafi"
+// console.log(person);
+// let a:string;
+// let b:number;
+// a="kdfjdf";
+// b="kjfdsjdf"
+// let a:(string| number)[]=[];
+// a.push(332);
+// a.push("hjadfjdfa");
+// console.log(a);
+// let a:string|number;
+// //a="kjdfhhfdj";
+// a=44564;
+// object........
+// let c:object;
+// c={
+//     name:"fdshdsgf",
+//     age:236,
+//     male:true,
+// }
+// console.log(c)
+// let c:{
+//     name:string,
+//     age:number,
+//     adult:boolean,
+// }
+// c={
+//     name:"Raj",
+//     age:25,
+//     adult:true,
+// }
+// console.log(c)
+// let c:object;
+// c=[1,2,3];
+// let a:any;
+// a=5;
+// a="hfds"
+// let a:any[]=[];
+// a.push(6544);
+// a.push("kjsdkfjkfdjs");
+// console.log(a);
+// let b:{
+//     name:any;
+//     age:any;
+// }
+// b={
+//     name:656545,
+//     age:4545,
+// }
+// console.log(b);
+// const myfun=()=>{
+//     console.log("hello");
+// }
+// console.log(myfun())
+// let myFun:Function;
+// myFun=()=>{
+//     console.log("Hello")
+// }
+// myFun=5;
+// const MyFunc=(a:string,b:string,c:string="ture")=>{
+//    return (`hello ${a} ${b}`);
+// }
+// console.log(MyFunc("Rajdip","Raj")); 
+// type aliases,,,,,,,,,
+// type stringOrNumber=string|number;
+// type UserType={ name:string,age:number}
+// let UserDetails=(id:stringOrNumber,user:UserType)=>{
+//     console.log(`User Id is${id},name is ${user.name
+//     } and Age ${user.age}`);
+// };
+// let sayHello=(user:UserType)=>{
+//     console.log(`hello${user.age>50? "sir":"Mr."}${user.name}`);
+// };
+// let Add:(x:number,y:number)=>number;
+// Add=(m:number,n:number)=>{
+//     return m+n;
+// }
+// console.log(Add(45,20))
+// let  Calculation:(x:number,y:number,z:string)=>number;
+// Calculation=(a:number,b:number,c:string)=>{
+//     if(c=='add'){
+//         return a+b;
+//     }else{
+//         return a-b;
+//     }
+// }
+// console.log(Calculation(45,20,'add'))
+// let UserDetails:(id:number| string,userinfo:{
+//     name:string,
+//     age:number,
+// })=>void;
+// UserDetails=(id:number|string,userinfo:{
+//     name:string,
+//     age:number,
+// })=>{
+//     return `User Id :${id} UserName${userinfo.name},User Age :${userinfo.age}`
+// }
+// console.log(UserDetails(1,"RAj",45))
+// class player{
+//     name:string;
+//     age:number;
+//     Country:string
+//     constructor(n:string,a:number,c:string){
+//         this.name=n,
+//         this.age=a,
+//         this.Country=c;
+//     }
+//     play(){
+//         console.log(`${this.name}from ${this.Country} age ${this.age}`)
+//     }
+// }
+// const Mashrafi= new player("Sakib",40,"Bangladesh");
+// const Sakib=new player("Mashrafi",35,"Bangladesh");
+// const players:player[]=[];
+// players.push(Sakib);
+// players.push(Mashrafi);
+// console.log (players);
+// class player {
+//     constructor(
+//         private name: string,
+//         public age: number,
+//         readonly Country: string
+//         ) 
+//     {
+//     }
+//     play() {
+//         console.log(`${this.name}from ${this.Country} age ${this.age}`)
+//     }
+// }
+// import{player} from './classes/player.js'
+// import{IsPlayer}from './interface/Iplayer.js'
+// const Mashrafi = new player("Sakib", 40, "Bangladesh");
+// let Sakib :IsPlayer;
+// Sakib=new player("abc",24,"Bangladesh")
+// const players: IsPlayer[] = [];
+// players.push(Sakib);
+// players.push(Mashrafi);
+// console.log(players);
+// interface RectangleOptions{
+//     width:number;
+//     length:number;
+// }
+// function drawRectangle(options:{
+//     width:number,
+//     length:number
+// }){
+//     let width=options.width;
+//     let length=options.length;
+// }
+// drawRectangle({
+//     width:30,
+//     length:20,
+// });
